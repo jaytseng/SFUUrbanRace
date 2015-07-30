116b19cab549ec6bd7ddf678407238fc55db1922
charlotte(laptop Acer) <yfc6@sfu.ca>
2015-07-30 13:52:14 -0700

    added JSON java for MySQL login
    getting error messge when trying to pass username and password

--- app/src/main/java/com/jaytseng/urbanrace/sfuurbanrace/Login.java
+++ app/src/main/java/com/jaytseng/urbanrace/sfuurbanrace/Login.java
@@ -1,38 +1,105 @@
 package com.jaytseng.urbanrace.sfuurbanrace;
-
-import android.support.v7.app.ActionBarActivity;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-
-public class Login extends ActionBarActivity {
-
+public class Login extends Activity implements OnClickListener{
+    private EditText input_username, input_password;
+    private Button buttonLogin;
+    // Progress Dialog
+   private ProgressDialog pDialog;
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
+    private static final String LOGIN_URL = "http://yourdomain.com/login.php";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        input_username = (EditText)findViewById(R.id.usernamelEditText);
+        input_password = (EditText)findViewById(R.id.passwordlEditText);
+        buttonLogin = (Button)findViewById(R.id.loginButton);
+        buttonLogin.setOnClickListener(this);
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginButton:
+                new AttemptLogin().execute();
+            default:
+                break;
+        }
     }
+    class AttemptLogin extends AsyncTask<String, String, String> {
+        /** *
+         *  Before starting background thread Show Progress Dialog * */
+        boolean failure = false;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Login.this);
+            pDialog.setMessage("Attempting for login...");
+            pDialog.setIndeterminate(false); pDialog.setCancelable(true);
+            pDialog.show();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // here Check for success tag
+            int success;
+            String username = input_username.getText().toString();
+            String password = input_password.getText().toString();
+            try {
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("request!", "starting");
+                JSONObject json = jsonParser.makeHttpRequest( LOGIN_URL, "POST", params);
+
+                // checking log for json response
+                Log.d("Login attempt", json.toString());
+                // success tag for json
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("Successfully Login!", json.toString());
+                    Intent ii = new Intent(Login.this,RaceListHome.class);
+
+                    // this finish() method is used to tell android os that we are done with current
+                    // activity now! Moving to other activity
+                    startActivity(ii);
+                    finish();
+                    return json.getString(TAG_MESSAGE);
+
+                    }else{
+                           return json.getString(TAG_MESSAGE);
+                } }
+            catch (JSONException e) {
+                e.printStackTrace();
+            } return null;
+        } /**
+         * Once the background process is done we need to Dismiss the progress dialog asap *
+         * **/
+        protected void onPostExecute(String message) {
+            pDialog.dismiss();
+            if (message != null){
+                Toast.makeText(Login.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
