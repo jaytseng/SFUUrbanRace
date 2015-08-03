bcaafe73e78c3c9663c4464848b738413e8d9057
charlotte(laptop Acer) <yfc6@sfu.ca>
2015-08-02 23:41:43 -0700

    changed register Page layout

--- app/src/main/java/com/jaytseng/urbanrace/sfuurbanrace/Register.java
+++ app/src/main/java/com/jaytseng/urbanrace/sfuurbanrace/Register.java
@@ -5,9 +5,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class Register extends Activity{
+public class Register extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
